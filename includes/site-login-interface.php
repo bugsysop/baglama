bf9b774ca3479fe7de3fac8cedc2b27bf772a309
Aris <aris@papatheodorou.net>
2022-08-12 11:03:24 +0300

    Customize login page

--- includes/site-login-interface.php
+++ includes/site-login-interface.php
@@ -2,13 +2,17 @@
 // Voir : https://css-tricks.com/snippets/wordpress/customize-login-page/
 // https://www.wpbeginner.com/plugins/how-to-create-custom-login-page-for-wordpress/
 // https://wpmudev.com/blog/customize-login-page/
-/*
-// Change the Logo
-function tmprs_custom_login_logo() {
-	echo '<style type="text/css">h1 a { background: url('.get_bloginfo('template_directory').'/assets/img/logo-login.gif) 50% 50% no-repeat !important; }</style>';
+
+// Custom style for login page only
+add_action('login_head', 'tmprs_custom_style');
+function tmprs_custom_style() {
+	echo '<!-- BAGLAMA -->';
+	echo '<style type="text/css">';
+	echo 'h1 a { background: url("' . esc_url( plugins_url('../assets/img/baglama-login-logo.png', __FILE__ ) ) . '") no-repeat !important; }';
+	echo '.login #nav {display: none;}';
+	echo '.login #backtoblog { text-align: center; }';
+	echo '</style>';
 }
-add_action('login_head', 'tmprs_custom_login_logo');
-*/
 
 // Login page logo link
 add_filter( 'login_headerurl', 'tmprs_login_logo_link' );
@@ -16,13 +20,15 @@ function tmprs_login_logo_link() {
 	return get_bloginfo( 'url' );
 }
 
-/*
 // Change the logo Title attribute
 function tmprs_change_login_logo_title() {
 	return get_option('blogname');
 }
 add_filter('login_headertitle', 'tmprs_change_login_logo_title');
 
-*/
+// Remove login page language switcher
+
+add_filter( 'login_display_language_dropdown', '__return_false' );
+
 
 
