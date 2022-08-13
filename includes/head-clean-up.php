9a44326f05575a18c3d66f800ab426fe296d1d13
Aris <aris@papatheodorou.net>
2022-08-13 12:53:24 +0300

    Switch to 'baglama_' prefix

--- includes/head-clean-up.php
+++ includes/head-clean-up.php
@@ -1,9 +1,9 @@
 <?php
-add_filter('the_generator', 'tmprs_remove_version');
-function tmprs_remove_version() {return '';}
-add_filter('style_loader_src', 'tmprs_remove_version_scripts_styles', 9999);
-add_filter('script_loader_src', 'tmprs_remove_version_scripts_styles', 9999);
-function tmprs_remove_version_scripts_styles($src){if(strpos($src,'ver=')){$src=remove_query_arg('ver',$src);}return $src;}
+add_filter('the_generator', 'baglama_remove_version');
+function baglama_remove_version() {return '';}
+add_filter('style_loader_src', 'baglama_remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'baglama_remove_version_scripts_styles', 9999);
+function baglama_remove_version_scripts_styles($src){if(strpos($src,'ver=')){$src=remove_query_arg('ver',$src);}return $src;}
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
