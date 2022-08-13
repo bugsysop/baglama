316e030d255889328a11d6327f76da16ae984f93
Aris <aris@papatheodorou.net>
2022-08-13 12:40:15 +0300

    Switch to 'baglama_' prefix & 'Not Active' header

--- includes/site-login-redirect.php
+++ includes/site-login-redirect.php
@@ -1,9 +1,14 @@
 <?php
+/*
+ * NOT ACTIVE - NOT TESTED
+ *
+ */
+
 // After login redinect non-admin users to home
-add_filter( 'login_redirect', 'tmprs_login_redirect', 10, 3 );
-function tmprs_login_redirect( $redirect_to, $request, $user ) {
-	$tmprs_roles = isset( $user->roles );
-	if ( is_array( $tmprs_roles ) && in_array( 'administrator', $tmprs_roles ) ) {
+add_filter( 'login_redirect', 'baglama_login_redirect', 10, 3 );
+function baglama_login_redirect( $redirect_to, $request, $user ) {
+	$baglama_roles = isset( $user->roles );
+	if ( is_array( $baglama_roles ) && in_array( 'administrator', $baglama_roles ) ) {
 		return admin_url();
 	} else {
 		return site_url();
