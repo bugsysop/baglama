9ef9fe98357ad1538cfc9c9611c1eacb9d5999f7
Aris <aris@papatheodorou.net>
2022-08-14 11:58:43 +0300

    Move admin footer text function

--- backoffice/admin-ui-tweaks.php
+++ backoffice/admin-ui-tweaks.php
@@ -1,16 +1,24 @@
 <?php
-/*
- * Note - Split this file ?
- *
- */
+
+// Welcome panel
 remove_action('welcome_panel', 'wp_welcome_panel');
+
+// WordPress logo
 add_action( 'wp_before_admin_bar_render', 'baglama_remove_wp_logo_admin', 0 );
 function baglama_remove_wp_logo_admin() {
 	global $wp_admin_bar;
 	$wp_admin_bar->remove_menu( 'wp-logo' );
 }
-add_filter('screen_options_show_screen', 'baglama_remove_screen_options');
-function baglama_remove_screen_options() { if(!current_user_can('manage_options')) { return false;} return true; }
-//add_filter('admin_footer_text', 'baglama_change_footer_admin');
-//function baglama_change_footer_admin () { echo 'Welcome to the Frontline!'; }
+
+// Admin footer
+// add_filter('admin_footer_text', 'baglama_admin_footer_text');
+// function baglama_admin_footer_text() { echo 'The kids are united: Wordpress & Bağlama Rule'; }
+add_filter('admin_footer_text', 'baglama_admin_footer_text');
+function baglama_admin_footer_text($text) {
+	$text = 'The kids are united: Wordpress & Bağlama Rule';
+  return $text;
+}
+
+
+
 
