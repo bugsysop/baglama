5f8d10f2393a9cf4c6e4324cee88a1779a87b243
Aris <_aris@noreply.codeberg.org>
2022-09-08 17:18:49 +0200

    Remove trailing slash (in case)

--- baglama.php
+++ baglama.php
@@ -6,7 +6,7 @@ Plugin URI: https://codeberg.org/_aris/baglama
 Description: Dispositif de publication expérimental basé sur WordPress
 Author: aris~
 Author URI: https://papatheodorou.net/
-GitHub Plugin URI: https://codeberg.org/_aris/baglama/
+GitHub Plugin URI: https://codeberg.org/_aris/baglama
 Primary Branch: main
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
