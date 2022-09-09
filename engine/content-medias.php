18c0728a33ce79c77c7e2f9487bd94450c439bb5
Aris <_aris@noreply.codeberg.org>
2022-09-09 11:07:26 +0200

    Cleaning code

--- engine/content-medias.php
+++ engine/content-medias.php
@@ -5,7 +5,7 @@ add_filter( 'sanitize_file_name', 'mb_strtolower' );
 
 /* 
  * Disable Media Permalink - 1.0
- * https://wordpress.org/plugins/disable-media-permalink-by-hardweb-it/
+ * @link https://wordpress.org/plugins/disable-media-permalink-by-hardweb-it/
  */
 add_filter( 'rewrite_rules_array', 'baglama_cleanup_attachment_permalink' );
 function baglama_cleanup_attachment_permalink( $rules ) {
