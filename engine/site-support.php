e0c4e7ab44a9820c697c40d883ba0133f0e41dc4
Aris <_aris@noreply.codeberg.org>
2022-09-09 12:46:27 +0200

    Remove WP Emoji DNS prefetch from page head

--- engine/site-support.php
+++ engine/site-support.php
@@ -12,4 +12,5 @@ remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
-
+// Remove WP Emoji DNS prefetch from page head
+add_filter( 'emoji_svg_url', '__return_false' );
