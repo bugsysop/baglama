eb4307220c73a35f4088d6d0b5b87742fe5783a5
Aris <_aris@noreply.codeberg.org>
2022-09-10 18:32:31 +0200

    Remove support for Wordpress 4.*

--- assets/js/slate.js
+++ assets/js/slate.js
@@ -3,69 +3,6 @@ jQuery(document).ready(function( $ ) {
 	var $body = $( 'body' );
 
 	$body.addClass( 'slate-admin-theme' );
-
-	// Move elements inside #post-body-content
-	// WordPress Version 4.0 - 4.2
-	if ( $body.is( '.branch-4' ) || $body.is( '.branch-4-0' ) || $body.is( '.branch-4-1' ) || $body.is( '.branch-4-2' ) ) {
-		$( '.wrap > h2, #screen-meta-links, #screen-meta' ).prependTo( '#post-body-content' );
-
-		// Move messages
-		if ( $( '.wrap > .updated, .wrap > .error' ).length != 0 && $( '#post-body-content' ).length != 0 ) {
-			$( '.wrap > .updated, .wrap > .error' ).insertBefore( '#post-body-content h2' );
-		}
-
-		// Move elements on Tags/Category pages
-		if ( $( '.edit-tags-php #col-right' ).length != 0 ) {
-			$( '.wrap > h2, .wrap > #ajax-response, .wrap > .search-form, .wrap > br' ).prependTo( '#col-right' );
-		}
-	}
-
-	// WordPress Version 4.3
-	if ( $body.is( '.branch-4-3' ) ) {
-		$( '.wrap > h1, #screen-meta-links, #screen-meta' ).prependTo( '#post-body-content' );
-
-		// Move messages
-		var $messages = $( '.wrap > .updated, .wrap > .error, .wrap > .notice, #wpbody-content > .updated, #wpbody-content > .error, #wpbody-content > .notice, #wpbody-content > .update-nag' );
-		if ( $messages.length != 0 && $( '#post-body-content' ).length != 0 ) {
-			$messages.insertBefore( '#post-body-content h1' );
-		}
-	}
-	if ( $body.is( '.edit-tags-php.branch-4-3' ) ) {
-		// Move elements on Tags/Category pages
-		$( '.wrap > h1, .wrap > #ajax-response, .wrap > .search-form, .wrap > br, .wrap > .updated, .wrap > .error, .wrap > .notice, #wpbody-content > .updated, #wpbody-content > .error, #wpbody-content > .notice' ).prependTo( '#col-right .col-wrap' );
-	}
-
-	// WordPress Version 4.4 or 4.5
-	if ( $body.is( '.branch-4-4' ) || $body.is( '.branch-4-5' ) ) {
-		// Move Elements
-		$( '.wrap > h1, #screen-meta-links, #screen-meta' ).prependTo( '#post-body-content' );
-	}
-	if ( $body.is( '.edit-tags-php.branch-4-4' ) || $body.is( '.edit-tags-php.branch-4-5' ) ) {
-		// Move elements on Tags/Category pages
-		$( '.wrap > h1, .wrap > #ajax-response, .wrap > .search-form, .wrap > br, .wrap > .updated, .wrap > .error' ).prependTo( '#col-right .col-wrap' );
-	}
-
-	// WordPress Version 4.6
-	if ( $body.is( '.branch-4-6' ) ) {
-		// Move Elements
-		$( '.wrap > h1, #screen-meta-links, #screen-meta' ).prependTo( '#post-body-content' );
-	}
-	if ( $body.is( '.edit-tags-php.branch-4-6' ) ) {
-		// Move elements on Tags/Category pages
-		$( '.wrap > h1, .wrap > #ajax-response, .wrap > .search-form, .wrap > br, .wrap > .updated, .wrap > .error' ).prependTo( '#col-right .col-wrap' );
-	}
-
-	// WordPress Version 4.7, 4.8, 4.9
-	if ( $body.is( '.branch-4-7' ) || $body.is( '.branch-4-8' ) || $body.is( '.branch-4-9' ) ) {
-		// Move Elements
-		$( '.wrap > h1, #screen-meta-links, #screen-meta' ).prependTo( '#post-body-content' );
-		// Move elements on Posts page
-		$( '.page-title-action' ).appendTo( '.wp-heading-inline' );
-	}
-	if ( $body.is( '.edit-tags-php.branch-4-7' ) || $body.is( '.edit-tags-php.branch-4-8' ) || $body.is( '.edit-tags-php.branch-4-9' ) ) {
-		// Move elements on Tags/Category pages
-		$( '.wrap > h1, .wrap > #ajax-response, .wrap > .search-form, .wrap > br, .wrap > .updated, .wrap > .error' ).prependTo( '#col-right .col-wrap' );
-	}
 	
 	// Add background divs
 	if ($('#poststuff #side-sortables').length != 0 && !$('body').is('.index-php')) {
