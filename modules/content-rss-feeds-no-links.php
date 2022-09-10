563e19ba2b7e50594859c242b2403c1affda0145
Aris <_aris@noreply.codeberg.org>
2022-09-10 18:47:08 +0200

    Add informations & instructions

--- modules/content-rss-feeds-no-links.php
+++ modules/content-rss-feeds-no-links.php
@@ -1,8 +1,11 @@
 <?php
 /*
- * NOT ACTIVE
+ * Not active by default
+ * Remove only the feeds links in page head, but they are still accessible
+ * To activate, add this call to main plugin file:
+ *     include_once plugin_dir_path( __FILE__ ).'modules/content-rss-feeds-no-links.php';
+ * It's also a good idea to remove or (better) comment this line:
+ *     // include_once plugin_dir_path( __FILE__ ).'modules/content-rss-feeds.php';
  */
-
-// Note: remove only the link in head
 remove_action( 'wp_head', 'feed_links', 2 );
 remove_action( 'wp_head', 'feed_links_extra', 3 );
