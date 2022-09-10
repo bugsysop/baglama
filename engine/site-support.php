05e29b8ad25db1096d6aedc3855ae4be177be861
Aris <_aris@noreply.codeberg.org>
2022-09-10 08:28:52 +0200

    Removing capital P dangit

--- engine/site-support.php
+++ engine/site-support.php
@@ -14,3 +14,8 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
 // Remove WP Emoji DNS prefetch from page head
 add_filter( 'emoji_svg_url', '__return_false' );
+
+// Removing capital P dangit
+remove_filter( 'the_title', 'capital_P_dangit', 11 );
+remove_filter( 'the_content', 'capital_P_dangit', 11 );
+remove_filter( 'comment_text', 'capital_P_dangit', 31 );
