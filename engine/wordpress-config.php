dcf5e2ac3fdd1101f9848c9c2142270983a44dc8
Aris <_aris@noreply.codeberg.org>
2022-09-22 17:48:32 +0200

    Move 'Capital P dangit' and 'Bye bye JQuery' to wordpress-config.php

--- engine/wordpress-config.php
+++ engine/wordpress-config.php
@@ -1,3 +1,13 @@
 <?php
+<?php
+// Removing capital P dangit
+remove_filter( 'the_title', 'capital_P_dangit', 11 );
+remove_filter( 'the_content', 'capital_P_dangit', 11 );
+remove_filter( 'comment_text', 'capital_P_dangit', 31 );
+
+// Bye bye JQuery from frontend
+add_action('wp_enqueue_scripts', 'baglama_deregister_jquery');
+function baglama_deregister_jquery() { if ( !is_admin() ) {wp_deregister_script('jquery');} } 
+
 // Defined in wp-config.php - Here in case
 // if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', false);
