fbb5e356959ca82dfa9befff64dfe6ea2cf8bb9e
Aris <_aris@noreply.codeberg.org>
2022-10-18 12:19:10 +0200

    Remove the color picker from user profile admin

--- backoffice/user-profile-tweaks.php
+++ backoffice/user-profile-tweaks.php
@@ -1,7 +1,6 @@
 <?php
 /*
- * Remove useless option from user profile
- * Note: hidden with CSS
+ * Simplify user profile admin
  */
 add_action( 'admin_head', function(){
     ob_start(); ?>
@@ -16,3 +15,13 @@ add_action( 'admin_head', function(){
     </style>
     <?php ob_end_flush();
 });
+
+/*
+ * Remove the color picker from the user profile admin
+ * Note: compatibility only with WP 6.0 ans later
+ * https://wordpress.org/support/topic/remove-color-picker-from-profile-edit-screen-in-wp-6-0/
+ */
+add_action( 'admin_head-profile.php', 'baglama_remove_admin_color_scheme_picker' );
+function baglama_remove_admin_color_scheme_picker() {
+    remove_action( 'admin_color_scheme_picker', 'admin_color_scheme_picker' );
+}
