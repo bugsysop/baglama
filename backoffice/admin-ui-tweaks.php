77fa4d41ab30198d2f7a58a8cabec2c2a3d1178d
Aris <_aris@noreply.codeberg.org>
2022-10-21 19:24:12 +0200

    Howdy? No more in admin bar

--- backoffice/admin-ui-tweaks.php
+++ backoffice/admin-ui-tweaks.php
@@ -10,6 +10,17 @@ function baglama_remove_wp_logo_admin() {
 	$wp_admin_bar->remove_menu( 'wp-logo' );
 }
 
+// Howdy? No more in admin bar
+add_action( 'admin_bar_menu', 'baglama_remove_howdy', 11 );
+function baglama_remove_howdy ( $wp_admin_bar ) {
+	$current_user = wp_get_current_user();
+	$avatar = get_avatar( $current_user->ID, 28 );
+	$wp_admin_bar->add_node( [
+		'id' => 'my-account',
+		'title' => $current_user->display_name . $avatar
+	] );
+}
+
 // Admin footer
 // add_filter('admin_footer_text', 'baglama_admin_footer_text');
 // function baglama_admin_footer_text() { echo 'The kids are united: Wordpress & Bağlama Rule'; }
