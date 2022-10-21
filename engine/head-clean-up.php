0e0685001fe540912d15cdc253889c88aef16975
Aris <_aris@noreply.codeberg.org>
2022-10-21 23:53:06 +0200

    Remove ajacent post prefetching

--- engine/head-clean-up.php
+++ engine/head-clean-up.php
@@ -11,3 +11,4 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 remove_action('wp_head', 'rest_output_link_wp_head', 10);
 remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
