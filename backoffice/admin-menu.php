3e65079d2c42ad325f7d54a9a823283947653f58
Aris <_aris@noreply.codeberg.org>
2022-10-25 12:51:37 +0200

    Customize admin menu order

--- backoffice/admin-menu.php
+++ backoffice/admin-menu.php
@@ -1,26 +1,39 @@
 <?php
 /*
- *
- * Examples
- * remove_menu_page( 'plugin_menu_slug' );
- * add_submenu_page('options-general.php','Page Title', 'Menu Label','manage_options', 'plugin_menu_slug' );
- *
- * References
- * @link https://developer.wordpress.org/reference/functions/remove_menu_page/
- * @link https://developer.wordpress.org/reference/functions/remove_submenu_page/
- * @link https://developer.wordpress.org/reference/functions/add_menu_page/
- * @link https://developer.wordpress.org/reference/functions/add_submenu_page/
- * @link https://wordpress.stackexchange.com/questions/306447/move-plugin-settings-to-settings-menu-in-the-admin
- * @link 
- *
+ * Customize admin menu
  */
 
+// Global
+function baglama_reorder_admin_menu( $__return_true ) {
+    return array(
+         'index.php', 					// Dashboard
+		 'separator1', 					// --Space--
+         'edit.php', 					// Posts
+		 'edit.php?post_type=page', 	// Pages
+         'upload.php', 					// Media
+		 'edit-comments.php', 			// Comments
+		 'separator2', 					// --Space--
+         'themes.php', 					// Appearance
+		 'plugins.php', 				// Plugins
+		 'tools.php', 					// Tools
+         'users.php', 					// Users     
+		 'separator3', 					// --Space-- NO !
+         'tools.php', 					// Tools
+         'options-general.php', 		// Settings
+		 'separator-last', 				// --Space-- Last separator
+   );
+}
+add_filter( 'custom_menu_order', 'baglama_reorder_admin_menu' );
+add_filter( 'menu_order', 'baglama_reorder_admin_menu' );
+
+// Themes en plugins files Edit
 add_action( 'admin_menu', 'baglama_remove_menu_pages', 999 );
 function baglama_remove_menu_pages() {
     remove_submenu_page( 'themes.php', 'theme-editor.php' );
     remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
 }
 
+// Plugins humility
 add_action( 'admin_menu', 'baglama_twentig_humility', 999 );
 function baglama_twentig_humility() {
     if ( is_plugin_active( 'twentig/twentig.php' ) ) {
@@ -28,9 +41,3 @@ function baglama_twentig_humility() {
     add_submenu_page('themes.php','Twentig Page', 'Twentig','manage_options', 'twentig' );
     }
 }
-
-/*
-add_action( 'admin_menu', 'baglama_add_menu_pages' );
-function baglama_add_menu_pages() {
-    add_submenu_page('themes.php','Twentig Page', 'Twentig','manage_options', 'twentig' );
-} */
