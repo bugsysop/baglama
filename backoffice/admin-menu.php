78cc22a948f02edce3824010de542f6b47ec87a6
Aris <_aris@noreply.codeberg.org>
2022-10-26 17:28:51 +0200

    Groink, groink... Code cleaning

--- backoffice/admin-menu.php
+++ backoffice/admin-menu.php
@@ -4,27 +4,25 @@
  */
 
 // Global
-function baglama_reorder_admin_menu( $__return_true ) {
-    return array(
-         'index.php',                   // Dashboard
-		 'separator1',                  // --Space--
-         'edit.php', 					// Posts
-		 'edit.php?post_type=page', 	// Pages
-         'upload.php', 					// Media
-		 'edit-comments.php', 			// Comments
-		 'separator2', 					// --Space--
-         'themes.php', 					// Appearance
-		 'plugins.php', 				// Plugins
-		 'tools.php', 					// Tools
-         'users.php', 					// Users     
-		 'separator3', 					// --Space-- NO !
-         'tools.php', 					// Tools
-         'options-general.php', 		// Settings
-		 'separator-last', 				// --Space-- Last separator
-   );
-}
 add_filter( 'custom_menu_order', 'baglama_reorder_admin_menu' );
 add_filter( 'menu_order', 'baglama_reorder_admin_menu' );
+function baglama_reorder_admin_menu( $__return_true ) {
+    return array(
+        'index.php',                   // Dashboard
+        'separator1',                  // --Space--
+        'edit.php', 					// Posts
+        'edit.php?post_type=page', 	    // Pages
+        'upload.php', 					// Media
+        'edit-comments.php', 			// Comments
+        'separator2', 					// --Space--
+        'themes.php', 					// Appearance
+        'plugins.php', 				    // Plugins
+        'tools.php', 					// Tools
+        'users.php', 					// Users     
+        'options-general.php', 		    // Settings
+        'separator-last', 				// --Space-- Last separator
+   );
+}
 
 add_action( 'admin_menu', 'baglama_move_to_submenu' );
 function baglama_move_to_submenu() {
