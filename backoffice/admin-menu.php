8db53f17b5051fa8a7c2d38408f122b7c4a48c5b
Aris <_aris@noreply.codeberg.org>
2022-11-01 14:33:34 +0100

    Add acces to block themes edition + better code for admin menu tweaks

--- backoffice/admin-menu.php
+++ backoffice/admin-menu.php
@@ -4,8 +4,9 @@
  */
 
 // Global
-add_filter( 'custom_menu_order', 'baglama_reorder_admin_menu' );
-add_filter( 'menu_order', 'baglama_reorder_admin_menu' );
+// Have to change this!
+add_filter( 'custom_menu_order', 'baglama_make_admin_menu' );
+add_filter( 'menu_order', 'baglama_make_admin_menu' );
 function baglama_reorder_admin_menu( $__return_true ) {
     return array(
         'index.php',                    // Dashboard
@@ -24,34 +25,37 @@ function baglama_reorder_admin_menu( $__return_true ) {
    );
 }
 
-add_action( 'admin_menu', 'baglama_add_submenus' );
-function baglama_add_submenus() {
-    // WP pages: About, Credits, Freedom, Privacy, etc
-    add_submenu_page( 'index.php', '', 'About',	'read', 'about.php');
-}
-
-add_action( 'admin_menu', 'baglama_move_to_submenu' );
-function baglama_move_to_submenu() {
-    // Reusable Blocks: create submenu under Apparence
-    add_submenu_page('themes.php',__('Reusable Blocks'), __('Reusable Blocks'),'manage_options', 'edit.php?post_type=wp_block' );
-}
-
-// Themes and plugins files Edit
+// Remove
 add_action( 'admin_menu', 'baglama_remove_menu_pages', 999 );
 function baglama_remove_menu_pages() {
-    // For WordPress 6.0.*
+    // For WordPress 6.0.* & Classic Themes
     remove_submenu_page( 'themes.php', 'theme-editor.php' );
     remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
-    // For WordPress 6.1-RC3
+    // For WordPress 6.1-RC3 & Classic Themes
     remove_submenu_page( 'tools.php', 'theme-editor.php' );
     remove_submenu_page( 'tools.php', 'plugin-editor.php' );
-    // For All
+    // For FSE Themes: added by Gutenberg?
     remove_menu_page( 'edit.php?post_type=wp_block' );
 }
 
+// Submenu
+add_action( 'admin_menu', 'baglama_make_submenu' );
+function baglama_make_submenu() {
+	// Dashboard
+	add_submenu_page( 'index.php',__('About'),__('About'),	'read', 'about.php','', '3');
+    // Apparence
+    add_submenu_page('themes.php',__('Reusable Blocks'),__('Reusable Blocks'),'manage_options', 'edit.php?post_type=wp_block', '', '9' );
+	// Only if it’s a Block Theme
+	if (current_theme_supports('block-templates')) {
+		add_submenu_page('themes.php',__('Navigation Menus'),__('Navigation Menus'),'manage_options', 'edit.php?post_type=wp_navigation', '', '9' );
+    	add_submenu_page('themes.php',__('Templates'),__('Templates'),'manage_options', 'site-editor.php?postType=wp_template', '', '10' );
+    	add_submenu_page('themes.php',__('Template Parts'),__('Template Parts'),'manage_options', 'site-editor.php?postType=wp_template_part', '', '11' );
+	}
+}
+
 // Plugins humility
-add_action( 'admin_menu', 'baglama_twentig_humility', 999 );
-function baglama_twentig_humility() {
+add_action( 'admin_menu', 'baglama_plugins_humility', 999 );
+function baglama_plugins_humility() {
     if ( is_plugin_active( 'twentig/twentig.php' ) ) {
     remove_menu_page( 'twentig' );
     add_submenu_page('themes.php','Twentig Page', 'Twentig','manage_options', 'twentig' );
