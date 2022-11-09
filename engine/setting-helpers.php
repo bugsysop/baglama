5a04bc620e4f7236ecb3697a9aa6ed05f388fee7
Aris <_aris@noreply.codeberg.org>
2022-11-09 05:35:55 +0100

    Functions for setting page (WIP)

---
+++ engine/setting-helpers.php
@@ -0,0 +1,17 @@
+// Check if option is Valid to string
+function baglama_check_valid_option($optionString, $validString = "")
+{
+    $baglama_tools_options = get_option('baglama_tools_option_name'); // Array of All Options
+    if ($validString == "") {
+        $validString = $optionString;
+    }
+    if (isset($baglama_tools_options[$optionString])) {
+        if ($validString == $optionString) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
