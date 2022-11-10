4652509efafd69cf8d8d19ecfa06cdccc3950433
Aris <_aris@noreply.codeberg.org>
2022-11-10 10:03:30 +0100

    This is an optional module

--- modules/content-author-archive.php
+++ modules/content-author-archive.php
@@ -1,7 +1,11 @@
 <?php
-/* 
- * Disable author archives - 1.3.1
+/*
+ * Bağlama Settings
+ * Option -- Disable author archives page
+ *
+ * Source: Disable author archives Plugin - 1.3.1
  * https://wordpress.org/plugins/disable-author-archives
+ *
  */
 add_action( 'template_redirect',function(){if(isset($_GET['author'])||is_author()){global $wp_query;$wp_query->set_404();status_header(404);nocache_headers();}},1);
 add_filter( 'user_row_actions',function($actions){if(isset($actions['view']))unset($actions['view']);return $actions;},PHP_INT_MAX,2);
