116ac7756f85275057e6680d1c03af35e28b2460
Aris <_aris@noreply.codeberg.org>
2022-11-10 10:00:20 +0100

    This is an optional module

--- modules/content-rss-feeds-no-links.php
+++ modules/content-rss-feeds-no-links.php
@@ -1,11 +1,8 @@
 <?php
 /*
- * Not active by default
- * Remove only the feeds links in page head, but they are still accessible
- * To activate, add this call to main plugin file:
- *     include_once plugin_dir_path( __FILE__ ).'modules/content-rss-feeds-no-links.php';
- * It's also a good idea to remove or (better) comment this line:
- *     // include_once plugin_dir_path( __FILE__ ).'modules/content-rss-feeds.php';
+ * Bağlama Settings
+ * Option -- Hide all the feeds links in pages head
+ *
  */
 remove_action( 'wp_head', 'feed_links', 2 );
 remove_action( 'wp_head', 'feed_links_extra', 3 );
