32913949832b61317007c52240ebbf9ed2b8de1f
Aris <_aris@noreply.codeberg.org>
2022-11-23 10:42:01 +0000

    Add PHP-MySQL versions and Open Source software support in 'At a Glance' Dasboard widget

--- backoffice/dashboard-widgets-tweaks.php
+++ backoffice/dashboard-widgets-tweaks.php
@@ -13,3 +13,20 @@ function baglama_remove_dashboard_meta() {
 	remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
 }
 
+// Display PHP and MySQL versions
+add_action( 'rightnow_end', 'baglama_display_php_mysql_versions' );
+function baglama_display_php_mysql_versions() {
+	echo
+		sprintf(
+			__( 'Running PHP %1$s and MySQL %2$s', 'baglama' ),
+			phpversion(),
+			$GLOBALS['wpdb']->db_version()
+		);
+}
+
+// Display support to Free and Open Source software
+add_action( 'rightnow_end', 'baglama_display_open_source' );
+function baglama_display_open_source() {
+	echo __( '<br>and lot of others <strong>free and open source</strong> software', 'baglama' );
+
+}
