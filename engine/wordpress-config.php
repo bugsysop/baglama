d59bce5028323e218e11e52e54cd4fcaecd756c7
Aris <_aris@noreply.codeberg.org>
2022-11-24 10:10:51 +0000

    Cleaning code

--- engine/wordpress-config.php
+++ engine/wordpress-config.php
@@ -5,8 +5,8 @@ remove_filter( 'the_content', 'capital_P_dangit', 11 );
 remove_filter( 'comment_text', 'capital_P_dangit', 31 );
 
 // Bye bye JQuery from frontend
-add_action('wp_enqueue_scripts', 'baglama_deregister_jquery');
-function baglama_deregister_jquery() { if ( !is_admin() ) {wp_deregister_script('jquery');} } 
+add_action( 'wp_enqueue_scripts', 'baglama_deregister_jquery' );
+function baglama_deregister_jquery() { if ( !is_admin() ) { wp_deregister_script( 'jquery' ); } } 
 
 // Defined in wp-config.php - Here in case
 // if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', false);
